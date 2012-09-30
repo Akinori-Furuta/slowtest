063f0e03d5fd77be148d66cf8d9f5c938d470d72
Akinori Furuta <furuta@usbserv.(none)>
2012-09-30 23:33:50 +0900

    Add gnuplot script.

--- extpart.sh
+++ extpart.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #spilt log file into sequential-write, random-read/write, and sequential-read parts.
 
+my_base=`basename "$0"`
+my_dir=`dirname "$0"`
+
 for f in $*
 do
 	ext=${f##*.}
@@ -16,11 +19,15 @@ do
 	fi
 
 	sw_file=${f%%.*}-sw.txt
+	sw_png=${f%%.*}-sw.png
 	sed -n '/Twrite/,/elp/ {p}' ${f} | grep '^[0-9][.]' | sed -n 's/,//gp' | sed -n 's/%//gp' > ${sw_file}
+	gnuplot -e "log_file=\"${sw_file}\"; load \"${my_dir}/sequential_write.gnuplot\"; quit" > ${sw_png}
 
 	ra_file=${f%%.*}-ra.txt
 	sed -n '/^i,[[:space:]]/,/Tread/ {p}' ${f} | grep '^[[:space:]]*[0-9]' > ${ra_file}
 
 	sr_file=${f%%.*}-sr.txt
+	sr_png=${f%%.*}-sr.png
 	sed -n '/Tread/,$ {p}' ${f} | grep '^[0-9][.]' | sed -n 's/,//gp' | sed -n 's/%//gp' > ${sr_file}
+	gnuplot -e "log_file=\"${sr_file}\"; load \"${my_dir}/sequential_read.gnuplot\"; quit" > ${sr_png}
 done
