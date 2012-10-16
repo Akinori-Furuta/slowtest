1e6deadb8ed25c3018f24029536bd5486c942315
Akinori Furuta <afuruta@m7.dion.ne.jp>
2012-10-17 02:00:20 +0900

    Add USB memory / card memory configuration with O_DIRECT.

---
+++ ssdtest_usbmems_direct.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Will be sourced by bash.
+
+export MAX_SECTORS_RANDOM_LONG=131072
+export MAX_SECTORS_RANDOM_MIDDLE=4096
+export MAX_SECTORS_RANDOM_SHORT=128
+export RANDOM_REPEATS=8192
+export DEF_CONFIG_RA_L="-py -xb -rn -my -dy -b 512 -i 1 -a ${MAX_SECTORS_RANDOM_LONG}   -n ${RANDOM_REPEATS}"
+export DEF_CONFIG_RA_M="-pn -xb -rn -my -dy -b 512 -i 1 -a ${MAX_SECTORS_RANDOM_MIDDLE} -n ${RANDOM_REPEATS}"
+export DEF_CONFIG_RA_S="-pn -xb -ry -my -dy -b 512 -i 1 -a ${MAX_SECTORS_RANDOM_SHORT}  -n ${RANDOM_REPEATS}"
