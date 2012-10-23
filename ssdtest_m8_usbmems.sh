f108bb33394c2960837eca16bf04870406f4fed7
Akinori Furuta <afuruta@m7.dion.ne.jp>
2012-10-24 01:11:21 +0900

    Add Sequential test blocks at one IO.

--- ssdtest_m8_usbmems.sh
+++ ssdtest_m8_usbmems.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # source this script.
 export SEQUENTIAL_DIRECT=y
+export SEQUENTIAL_BLOCKS=16384
 export RANDOM_MAX_BLOCKS_MAG=8
 export RANDOM_MAX_BLOCKS_BASE=128
 export RANDOM_MAX_BLOCKS_LEVEL=4
