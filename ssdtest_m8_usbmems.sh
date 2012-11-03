3d71d8c23af0bfb79d2103020e02d7dacc41e4cf
Akinori Furuta <afuruta@m7.dion.ne.jp>
2012-11-03 11:32:51 +0900

    Renames: Adopt M4L8 version test scripts as regular.

--- ssdtest_m8_usbmems.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# source this script.
-export SEQUENTIAL_DIRECT=y
-export SEQUENTIAL_BLOCKS=16384
-export RANDOM_MAX_BLOCKS_MAG=8
-export RANDOM_MAX_BLOCKS_BASE=128
-export RANDOM_MAX_BLOCKS_LEVEL=4
