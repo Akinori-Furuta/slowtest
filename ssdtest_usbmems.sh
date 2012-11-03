5f467df6a3b6160988bf2f9e6ecb0e1e786b23c7
Akinori Furuta <afuruta@m7.dion.ne.jp>
2012-11-03 15:17:53 +0900

    Specification change: Offer script wrapper test and plot tools for USB and Card memories.

--- ssdtest_usbmems.sh
+++ ssdtest_usbmems.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# source this script.
+# This script warap ssdtest.sh
+
+my_base=`basename "$0"`
+my_dir=`dirname "$0"`
+
 export SEQUENTIAL_DIRECT=y
 export SEQUENTIAL_BLOCKS=16384
 export RANDOM_MAX_BLOCKS_MAG=8
 export RANDOM_MAX_BLOCKS_BASE=128
 export RANDOM_MAX_BLOCKS_LEVEL=4
+
+${my_dir}/ssdtest.sh $*
