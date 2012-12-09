70da16dbf6d95b28c601a9640d78e782dca839fe
Akinori Furuta <afuruta@m7.dion.ne.jp>
2012-12-10 00:25:55 +0900

    Fix: help message typo.

--- ssdtestloop.sh
+++ ssdtestloop.sh
@@ -39,7 +39,7 @@ function Help() {
 	#     0         1         2         3         4         5         6         7
 	#     01234567890123456789012345678901234567890123456789012345678901234567890123456789
 	echo "Run test SSD performance loop."
-	echo "$0 [-L OptionalLabel] [-h] [-n num] test_file_or_directory"
+	echo "$0 [-L OptionalLabel] [-h] [-C num] test_file_or_directory"
 	echo "-L OptionalLabel : Jam string into log directory path."
 	echo "-C num : Run test num times."
 	echo "-h : Show this help."
