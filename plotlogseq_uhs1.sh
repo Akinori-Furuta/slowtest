002a7a9c9388e811fb941b04d42cebfb570bfe79
Akinori Furuta <afuruta@m7.dion.ne.jp>
2015-07-04 10:01:30 +0900

    Add UHS-1 SD Card plot script.

---
+++ plotlogseq_uhs1.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Plot for USB / Card memory plot settings.
+# this script wraps plotlogseq.sh
+#
+#  Copyright 2012 Akinori Furuta<afuruta@m7.dion.ne.jp>.
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+#  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+#  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+#  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+my_base=`basename "$0"`
+my_dir=`dirname "$0"`
+
+export	SEQUENTIAL_TRANSFER_SPEED_MIN="0.0"
+export	SEQUENTIAL_TRANSFER_SPEED_MAX="1.0e+8"
+
+export	RANDOM_TRANSFER_SPEED_MIN="1.0e+3"
+export	RANDOM_TRANSFER_SPEED_MAX="1.0e+10"
+
+${my_dir}/plotlogseq.sh $*
